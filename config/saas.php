0a487e379504e91952eaeb8bceb789091b41c1c7
Alex Renoki <rennokki@gmail.com>
2020-07-20 12:20:44 +0300

    Added config info

--- config/saas.php
+++ config/saas.php
@@ -2,11 +2,35 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Cashier Register Models
+    |--------------------------------------------------------------------------
+    |
+    | Here you can configure the model classes to use for the tables
+    | provided by this package. For example, you can extend the
+    | original model and make your needed changes then replace the
+    | following models with your extended ones to be used by the package.
+    |
+    */
+
     'models' => [
 
         'usage' => \RenokiCo\CashierRegister\Models\Usage::class,
 
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cashier Stripe Models
+    |--------------------------------------------------------------------------
+    |
+    | Here you can configure the model classes to use for the tables
+    | provided by Laravel Cashier. The models are already extended by
+    | Cashier Register, but you can extend them again if you need
+    | to customize them for your needs.
+    |
+    */
 
     'cashier' => [
 
